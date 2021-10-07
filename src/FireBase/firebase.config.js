a6cf2363c4cdb0f60ff95bb7ac57dc209f429c54
MinhajulAkib <00akibswe@gmail.com>
2021-10-07 11:19:07 +0600

    firebase-authentication

---
+++ src/FireBase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDs_X7obC4I8n-qh7Psj77O0fVtJh4Ff8Y",
+    authDomain: "simple-firebase-authenti-6b84b.firebaseapp.com",
+    projectId: "simple-firebase-authenti-6b84b",
+    storageBucket: "simple-firebase-authenti-6b84b.appspot.com",
+    messagingSenderId: "183199605503",
+    appId: "1:183199605503:web:286a78f5da3065e46362ca"
+  };
+
+  export default firebaseConfig;
